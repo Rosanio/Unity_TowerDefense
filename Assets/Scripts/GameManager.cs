e6ea112aa6f12390c59b6d9030a73a4845c67180
RosanioAccepto <matt.rosanio@acceptto.com>
2018-06-16 16:20:36 -0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+	public GameObject bullet;
+	public GameObject enemy;
+	private bool isBulletOnScreen = false;
+
+	void Awake() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject testBullet = Instantiate(bullet, new Vector3(-5f, 0f, 0f), Quaternion.identity) as GameObject;
+		GameObject testEnemy = Instantiate(enemy, new Vector3(10f, 10f, 0f), Quaternion.identity) as GameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
