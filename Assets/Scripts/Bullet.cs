79d32f3b9daacc1dc7d10a85e38d9836b4542f8c
RosanioAccepto <matt.rosanio@acceptto.com>
2018-06-18 18:29:31 -0700

    Update gitignore

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+	public float speed;
+
+	private Rigidbody2D rigidBody;
+	private Transform target = null;
+	private Vector2 position;
+
 	// Use this for initialization
 	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		position = GetComponent<Transform>().position;
+		FindTarget();
+	}
 
+	private void FindTarget() {
+		float minDistance = -1.0f;
+		for(int i = 0; i < GameManager.instance.enemies.Count; i++) {
+			Vector2 enemyPosition = GameManager.instance.enemies[i].position;
+			float distance = Vector2.Distance(position, enemyPosition);
+			if(minDistance < 0 || distance < minDistance) {
+				minDistance = distance;
+				target = GameManager.instance.enemies[i];
+			}
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//Detect if enemy is on screen
-
+		position = GetComponent<Transform>().position;
 		//Calculate new trajectory
+		Vector2 newVelocity = new Vector2((target.position.x - position.x), (target.position.y - position.y));
+		newVelocity.Normalize();
 
 		//Move
+		rigidBody.velocity = newVelocity * speed;
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,27 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance = null;
 	public GameObject bullet;
 	public GameObject enemy;
-	private bool isBulletOnScreen = false;
+
+	[HideInInspector] public List<Transform> bullets;
+	[HideInInspector] public List<Transform> enemies;
 
 	void Awake() {
-
+		if(instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
 	}
 
 	// Use this for initialization
 	void Start () {
-		GameObject testBullet = Instantiate(bullet, new Vector3(-5f, 0f, 0f), Quaternion.identity) as GameObject;
 		GameObject testEnemy = Instantiate(enemy, new Vector3(10f, 10f, 0f), Quaternion.identity) as GameObject;
+		enemies.Add(testEnemy.transform);
+		GameObject testEnemy2 = Instantiate(enemy, new Vector3(5f, 10f, 0f), Quaternion.identity) as GameObject;
+		enemies.Add(testEnemy2.transform);
+		GameObject testBullet = Instantiate(bullet, new Vector3(-5f, 0f, 0f), Quaternion.identity) as GameObject;
+		bullets.Add(testBullet.transform);
 	}
 
 	// Update is called once per frame
