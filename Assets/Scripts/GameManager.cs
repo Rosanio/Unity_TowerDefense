474485767b819d6c39863cb0fb7453f81349f29a
RosanioAccepto <matt.rosanio@acceptto.com>
2018-06-20 22:36:22 -0700

    Fire bullet when enemy approaches tower

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,11 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance = null;
 	public GameObject bullet;
 	public GameObject enemy;
+	public GameObject tower;
 
 	[HideInInspector] public List<Transform> bullets;
 	[HideInInspector] public List<Transform> enemies;
+	[HideInInspector] public List<Transform> towers;
 
 	void Awake() {
 		if(instance == null) {
@@ -19,18 +21,10 @@ public class GameManager : MonoBehaviour {
 		DontDestroyOnLoad(gameObject);
 	}
 
-	// Use this for initialization
 	void Start () {
-		GameObject testEnemy = Instantiate(enemy, new Vector3(10f, 10f, 0f), Quaternion.identity) as GameObject;
+		GameObject testTower = Instantiate(tower, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+		towers.Add(testTower.transform);
+		GameObject testEnemy = Instantiate(enemy, new Vector3(0f, 100f, 0f), Quaternion.identity) as GameObject;
 		enemies.Add(testEnemy.transform);
-		GameObject testEnemy2 = Instantiate(enemy, new Vector3(5f, 10f, 0f), Quaternion.identity) as GameObject;
-		enemies.Add(testEnemy2.transform);
-		GameObject testBullet = Instantiate(bullet, new Vector3(-5f, 0f, 0f), Quaternion.identity) as GameObject;
-		bullets.Add(testBullet.transform);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
 	}
 }
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	private CircleCollider2D collider;
+
+	void Start () {
+		collider = GetComponent<CircleCollider2D>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Enemy") {
+			spawnBullet();
+		}
+	}
+
+	private void spawnBullet() {
+		Instantiate(GameManager.instance.bullet,GetComponent<Transform>().position, Quaternion.identity);
+	}
+}
